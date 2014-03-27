d21f799acddf15d9c2c9e21db496b14b2e8a5ec8
Ujjwal Kanth <ujjwal.kanth@unbxd.com>
2014-03-27 21:54:43 +0530

    A backup commit

--- hub/src/HTTPHandler/HTTPHandler.go
+++ hub/src/HTTPHandler/HTTPHandler.go
@@ -5,6 +5,8 @@ import(
 	"log"
 	"memcached"
 	"encoding/json"
+	"github.com/gorilla/mux"
+	"errors"
 )
 
 
@@ -27,10 +29,15 @@ func Auth(w http.ResponseWriter,r *http.Request){
 
 
 func Add (w http.ResponseWriter,r *http.Request , ch chan map[string]interface{}){
-	action := r.FormValue("action")
+	err := validateRequest(w,r)
+	if err != nil{
+		return
+	}
+	vars := mux.Vars(r)
+	action := vars["action"]
 	var f interface{}
 	// We get the value as formValue, with a prefix of 'products'
-	err := json.Unmarshal([]byte(r.FormValue("input")), &f)
+	err = json.Unmarshal([]byte(r.FormValue("input")), &f)
 	if err != nil {
 		log.Panic("Invalid Json data sent" + err.Error())
 		http.Error(w,"{\"status\":\"Invalid Data\"}" , 400)
@@ -43,13 +50,28 @@ func Add (w http.ResponseWriter,r *http.Request , ch chan map[string]interface{}
 
 
 
-func validateRequest(r *http.Request){
+func validateRequest(w http.ResponseWriter,r *http.Request) error{
 	// Make a request to memcached and get the token based on key
 	user := r.FormValue("user")
 	token := r.FormValue("token")
 	if user != "" && token != "" {
 		// Check it the token exists in memcached
-
+		val, err := memcached.Get(user)
+		if err != nil{
+			// Throw panic
+			log.Panic()
+		}else{
+			if val != token{
+				// Valid request
+				// Break the request
+				http.Error(w,"{\"status\":\"Logged Out\"}" , 415)
+				return  errors.New("Logged Out")
+			}
+		}
+	}else{
+		http.Error(w,"{\"status\":\"Bad Request\"}" , 400)
+		return errors.New("Bad Request")
 	}
+	return nil
 }
 
