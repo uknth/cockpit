e7bc9edf17a4b8b0931c92c3c7c89816c88fe41d
Anil Kumar <anil1095@gmail.com>
2014-03-28 19:51:52 +0530

    auth controller changes

--- php/application/controllers/auth.php
+++ php/application/controllers/auth.php
@@ -145,6 +145,47 @@ class Auth extends CI_Controller {
 	}
 
 	function test(){
-		//post_to_go('add','user',array(),array('USER_ID' => $user_id, 'TOKEN' => $random_string));
+		// Replace with real server API key from Google APIs          
+        $apiKey = "AIzaSyCNJeYio3_-9tRF8fjuUep7BeV0c0rE8TU";
+        
+        // Replace with real client registration IDs
+        $registrationIDs = array( "APA91bGvKhOyHmvldS2yegp3gJ8swrnvGLTGHmybI7CK4Ui5o-A2un2o-ztegeuwX5zf3W1R96FU8E8t4rMX2DooTmAblc_yjCBYUsuQ5AIrNO-_gnxBQkSwPE0NiTEn5OpLmzDHo1xpjeL3iVXhePuJZaeFoAZs3-wRTCqgc9iq_UDioTrDv0E");
+        
+        $url = 'https://android.googleapis.com/gcm/send';
+		$serverApiKey = $apiKey;
+		$reg = "APA91bHaslUVsUrVDsUpOZ-FJ7AMUaWXnh7kfG4ztpwVVbztBSZcu55kG24F4xMl4qEBASZlCyYGsyP_f9-nsIAwbw-Lhb1bjZa1FKIzSA7-4cw39w2gwg00uyywM7xJSupx6AUG8EX-3fbiWtyFPoCv5A1IK3XIVG6cQKmoRm_roBfK1On-Chk"; // registration id
+
+		$headers = array(
+		'Content-Type:application/json',
+		'Authorization:key=' . $serverApiKey
+		);
+
+		$data = array(
+		'registration_ids' => array($reg),
+		'data' => array(
+		'message' => 'Hello, World!'
+		));
+
+		print (json_encode($data) . "\n\n");
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		if ($headers)
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+		$response = curl_exec($ch);
+
+		$header = curl_getinfo ( $ch );
+
+
+		curl_close($ch);
+
+		print_r ($response);
+
+		echo '<pre>'; print_r($header);echo '</pre>';
 	}
 }
